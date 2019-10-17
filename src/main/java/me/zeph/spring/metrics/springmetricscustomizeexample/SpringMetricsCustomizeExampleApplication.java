2932c6550b85fe2a024b6ab7f9b799e0b7cd18ad
benweizhu <benwei.zhu@exxonmobil.com>
2019-10-17 11:25:37 +0700

    init

---
+++ src/main/java/me/zeph/spring/metrics/springmetricscustomizeexample/SpringMetricsCustomizeExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.metrics.springmetricscustomizeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetricsCustomizeExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMetricsCustomizeExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zeph/spring/metrics/springmetricscustomizeexample/SpringMetricsCustomizeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.metrics.springmetricscustomizeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMetricsCustomizeExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
